b38e04624ce64f5079f23671cd8b476d2741374c
Elmue <elmue@gmx.de>
2025-07-08 14:29:05 -0300

    Add files via upload

--- SourceCode/Operations/Mathematical.cs
+++ SourceCode/Operations/Mathematical.cs
@@ -40,6 +40,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Text;
+using System.Globalization;
 using System.Windows.Forms;
 
 using OperationManager  = Operations.OperationManager;
@@ -55,15 +56,24 @@ namespace Operations
 {
     public partial class Mathematical : Form, IOperation
     {
+        // Single Channel Analog
+        const String MATH_FACTOR      = "Multiply with factor";
+        const String MATH_OFFSET      = "Add Offset";
+        // Dual Channel Analog
+        const String MATH_ADD_AB      = "Add Channel A + B";
         const String MATH_SUBTRACT_AB = "Subtract Channel A - B";
         const String MATH_SUBTRACT_BA = "Subtract Channel B - A";
+        const String MATH_MULTI_AB    = "Multiply Channel A x B";
+        // Dual Channel Digital
         const String MATH_LOGIC_AND   = "Logical AND";
         const String MATH_LOGIC_OR    = "Logical OR";
         const String MATH_LOGIC_XOR   = "Logical XOR";
 
         Channel mi_ChannelA;
         Channel mi_ChannelB;
+        Channel mi_Result;
         bool    mb_Analog;
+        bool    mb_Single;
 
         /// <summary>
         /// Implementation of interface IOperation
@@ -76,8 +86,8 @@ namespace Operations
             if (b_Analog && Utils.OsziPanel.CommonAnalogDrawing)
                 return; // Analog channels cannot be distinguished while drawn one on top of the other
 
-            if (OsziPanel.CurCapture.mi_Channels.Count < 2)
-                return; // at least 2 channels are required for math
+            if (!b_Analog && OsziPanel.CurCapture.ms32_DigitalCount < 2)
+                return; // at least 2 digital channels are required for AND, OR, XOR
 
             GraphMenuItem i_Item = new GraphMenuItem();
             i_Item.ms_MenuText  = "Math Operations";
@@ -104,8 +114,15 @@ namespace Operations
 
             if (mb_Analog)
             {
-                comboMath.Items.Add(MATH_SUBTRACT_AB);
-                comboMath.Items.Add(MATH_SUBTRACT_BA);
+                if (OsziPanel.CurCapture.mi_Channels.Count > 1)
+                {
+                    comboMath.Items.Add(MATH_ADD_AB);
+                    comboMath.Items.Add(MATH_SUBTRACT_AB);
+                    comboMath.Items.Add(MATH_SUBTRACT_BA);
+                    comboMath.Items.Add(MATH_MULTI_AB);
+                }
+                comboMath.Items.Add(MATH_FACTOR);
+                comboMath.Items.Add(MATH_OFFSET);
             }
             else // Digital
             {
@@ -116,9 +133,15 @@ namespace Operations
 
             lblMathMode.Text = mb_Analog ? "Analog" : "Digital";
 
+            // Dual
             lblNameA   .Text      = mi_ChannelA.ms_Name;
             lblNameA   .ForeColor = OsziPanel.GetChannelColor(mi_ChannelA);
             lblChannelA.ForeColor = lblNameA.ForeColor;
+
+            // Single
+            lblName    .Text      = lblNameA.Text;
+            lblName    .ForeColor = lblNameA.ForeColor;
+            lblChannel .ForeColor = lblNameA.ForeColor;
 
             foreach (Channel i_ChannelB in OsziPanel.CurCapture.mi_Channels)
             {
@@ -128,6 +151,34 @@ namespace Operations
 
             comboChannelB.SelectedIndex = 0;
             comboMath.SelectedIndex     = 0;
+        }
+
+        private void comboMath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mb_Single = false;
+            switch (comboMath.Text)
+            {
+                case MATH_FACTOR:
+                    mb_Single = true;
+                    lblParam.Text = "Factor:";
+                    lblUnit .Text = "";
+                    break;
+                case MATH_OFFSET:
+                    mb_Single = true;
+                    lblParam.Text = "Offset:";
+                    lblUnit .Text = "Volt";
+                    break;
+            }
+
+            groupSingle.Visible =  mb_Single;
+            groupDual  .Visible = !mb_Single;
+
+            GroupBox i_GroupVisible = mb_Single ? groupSingle : groupDual;
+            i_GroupVisible.Top = lblMathMode.Bottom + 10;
+
+            Size k_Client   = ClientSize;
+            k_Client.Height = i_GroupVisible.Bottom + 45;
+            ClientSize      = k_Client;
         }
 
         private void linkHelp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -153,65 +204,74 @@ namespace Operations
         {
             try
             {
-                if (mb_Analog)
-                {
-                    if (mi_ChannelA.mf_Analog == null || mi_ChannelB.mf_Analog == null)
-                    {
-                        MessageBox.Show(this, "If you right-click on an analog channel you must select two analog channels.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                }
-                else // digital
-                {
-                    if (mi_ChannelA.mu8_Digital == null || mi_ChannelB.mu8_Digital == null)
-                    {
-                        MessageBox.Show(this, "If you right-click on a digital channel you must select two digital channels.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                }
+                Prepare(); // throws
 
-                Channel i_Result = OsziPanel.CurCapture.FindOrCreateChannel(textResult.Text, mi_ChannelA);
-                int  s32_Samples = OsziPanel.CurCapture.ms32_Samples;
+                int s32_Samples = OsziPanel.CurCapture.ms32_Samples;
 
                 if (mb_Analog)
                 {
-                    i_Result.mf_Analog = new float[s32_Samples];
-                    i_Result.mi_SampleMinMax.mb_AnalogOK = false; // must be re-calculated
-
                     switch (comboMath.Text)
                     {
-                        case MATH_SUBTRACT_AB:
+                        case MATH_ADD_AB: // dual
                             for (int S=0; S<s32_Samples; S++)
-                                i_Result.mf_Analog[S] = mi_ChannelA.mf_Analog[S] - mi_ChannelB.mf_Analog[S];
+                                mi_Result.mf_Analog[S] = mi_ChannelA.mf_Analog[S] + mi_ChannelB.mf_Analog[S];
                             break;
 
-                        case MATH_SUBTRACT_BA:
+                        case MATH_SUBTRACT_AB: // dual
                             for (int S=0; S<s32_Samples; S++)
-                                i_Result.mf_Analog[S] = mi_ChannelB.mf_Analog[S] - mi_ChannelA.mf_Analog[S];
+                                mi_Result.mf_Analog[S] = mi_ChannelA.mf_Analog[S] - mi_ChannelB.mf_Analog[S];
+                            break;
+
+                        case MATH_SUBTRACT_BA: // dual
+                            for (int S=0; S<s32_Samples; S++)
+                                mi_Result.mf_Analog[S] = mi_ChannelB.mf_Analog[S] - mi_ChannelA.mf_Analog[S];
+                            break;
+
+                        case MATH_MULTI_AB: // dual
+                            for (int S=0; S<s32_Samples; S++)
+                                mi_Result.mf_Analog[S] = mi_ChannelA.mf_Analog[S] * mi_ChannelB.mf_Analog[S];
+                            break;
+
+                        case MATH_FACTOR: // single
+                            float f_Factor;
+                            if (!float.TryParse(textParameter.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out f_Factor))
+                                throw new Exception("Enter a valid factor.");
+
+                            for (int S=0; S<s32_Samples; S++)
+                                mi_ChannelA.mf_Analog[S] *= f_Factor;
+
+                            mi_ChannelA.mi_SampleMinMax.mb_AnalogOK = false;
+                            break;
+
+                        case MATH_OFFSET: // single
+                            float f_Offset;
+                            if (!float.TryParse(textParameter.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out f_Offset))
+                                throw new Exception("Enter a valid offset.");
+
+                            for (int S=0; S<s32_Samples; S++)
+                                mi_ChannelA.mf_Analog[S] += f_Offset;
+
+                            mi_ChannelA.mi_SampleMinMax.mb_AnalogOK = false;
                             break;
                     }
                 }
                 else // Digital (Only bit 0 is used)
                 {
-                    i_Result.mu8_Digital = new Byte[s32_Samples];
-                    i_Result.mi_MarkRows = null;
-                    i_Result.mi_SampleMinMax.mb_DigitalOK = false; // must be re-calculated
-
                     switch (comboMath.Text)
                     {
-                        case MATH_LOGIC_AND:
+                        case MATH_LOGIC_AND: // dual
                             for (int S=0; S<s32_Samples; S++)
-                                i_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] & mi_ChannelB.mu8_Digital[S]) & 0x01);
+                                mi_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] & mi_ChannelB.mu8_Digital[S]) & 0x01);
                             break;
 
-                        case MATH_LOGIC_OR:
+                        case MATH_LOGIC_OR: // dual
                             for (int S=0; S<s32_Samples; S++)
-                                i_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] | mi_ChannelB.mu8_Digital[S]) & 0x01);
+                                mi_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] | mi_ChannelB.mu8_Digital[S]) & 0x01);
                             break;
 
-                        case MATH_LOGIC_XOR: // This is the same as subtracting digital A - B  or  B - A
+                        case MATH_LOGIC_XOR: // dual (This is the same as subtracting A - B  or  B - A)
                             for (int S=0; S<s32_Samples; S++)
-                                i_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] ^ mi_ChannelB.mu8_Digital[S]) & 0x01);
+                                mi_Result.mu8_Digital[S] = (Byte)((mi_ChannelA.mu8_Digital[S] ^ mi_ChannelB.mu8_Digital[S]) & 0x01);
                             break;
                     }
                 }
@@ -224,6 +284,35 @@ namespace Operations
             catch (Exception Ex)
             {
                 Utils.ShowExceptionBox(this, Ex);
+            }
+        }
+
+        void Prepare()
+        {
+            mi_Result = null;
+
+            if (mb_Single)
+                return;
+
+            int s32_Samples = OsziPanel.CurCapture.ms32_Samples;
+            mi_Result       = OsziPanel.CurCapture.FindOrCreateChannel(textResult.Text, mi_ChannelA);
+            
+            if (mb_Analog)
+            {
+                if (mi_ChannelA.mf_Analog == null || mi_ChannelB.mf_Analog == null)
+                    throw new Exception("If you right-click on an analog channel you must select two analog channels.");
+
+                mi_Result.mf_Analog = new float[s32_Samples];
+                mi_Result.mi_SampleMinMax.mb_AnalogOK = false; // must be re-calculated
+            }
+            else // digital
+            {
+                if (mi_ChannelA.mu8_Digital == null || mi_ChannelB.mu8_Digital == null)
+                    throw new Exception("If you right-click on a digital channel you must select two digital channels.");
+
+                mi_Result.mu8_Digital = new Byte[s32_Samples];
+                mi_Result.mi_MarkRows = null;
+                mi_Result.mi_SampleMinMax.mb_DigitalOK = false; // must be re-calculated
             }
         }
     }
--- SourceCode/Operations/Mathematical.designer.cs
+++ SourceCode/Operations/Mathematical.designer.cs
@@ -42,15 +42,25 @@
             this.lblMathMode = new System.Windows.Forms.Label();
             this.btnExecuteClose = new System.Windows.Forms.Button();
             this.linkHelp = new System.Windows.Forms.LinkLabel();
+            this.lblParam = new System.Windows.Forms.Label();
+            this.textParameter = new System.Windows.Forms.TextBox();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.groupSingle = new System.Windows.Forms.GroupBox();
+            this.groupDual = new System.Windows.Forms.GroupBox();
+            this.lblChannel = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.groupSingle.SuspendLayout();
+            this.groupDual.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnExecute
             // 
+            this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnExecute.ForeColor = System.Drawing.Color.Black;
-            this.btnExecute.Location = new System.Drawing.Point(133, 143);
+            this.btnExecute.Location = new System.Drawing.Point(139, 269);
             this.btnExecute.Name = "btnExecute";
-            this.btnExecute.Size = new System.Drawing.Size(61, 23);
-            this.btnExecute.TabIndex = 5;
+            this.btnExecute.Size = new System.Drawing.Size(66, 23);
+            this.btnExecute.TabIndex = 51;
             this.btnExecute.Text = "Execute";
             this.btnExecute.UseVisualStyleBackColor = true;
             this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
@@ -59,15 +69,17 @@
             // 
             this.comboMath.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboMath.FormattingEnabled = true;
-            this.comboMath.Location = new System.Drawing.Point(103, 81);
+            this.comboMath.Location = new System.Drawing.Point(113, 11);
             this.comboMath.Name = "comboMath";
             this.comboMath.Size = new System.Drawing.Size(163, 21);
-            this.comboMath.TabIndex = 6;
+            this.comboMath.TabIndex = 20;
+            this.comboMath.SelectedIndexChanged += new System.EventHandler(this.comboMath_SelectedIndexChanged);
             // 
             // lblChannelA
             // 
             this.lblChannelA.AutoSize = true;
-            this.lblChannelA.Location = new System.Drawing.Point(12, 11);
+            this.lblChannelA.ForeColor = System.Drawing.Color.White;
+            this.lblChannelA.Location = new System.Drawing.Point(8, 22);
             this.lblChannelA.Name = "lblChannelA";
             this.lblChannelA.Size = new System.Drawing.Size(86, 13);
             this.lblChannelA.TabIndex = 7;
@@ -76,7 +88,8 @@
             // lblChannelB
             // 
             this.lblChannelB.AutoSize = true;
-            this.lblChannelB.Location = new System.Drawing.Point(12, 33);
+            this.lblChannelB.ForeColor = System.Drawing.Color.White;
+            this.lblChannelB.Location = new System.Drawing.Point(8, 50);
             this.lblChannelB.Name = "lblChannelB";
             this.lblChannelB.Size = new System.Drawing.Size(86, 13);
             this.lblChannelB.TabIndex = 8;
@@ -85,7 +98,8 @@
             // lblNameA
             // 
             this.lblNameA.AutoSize = true;
-            this.lblNameA.Location = new System.Drawing.Point(103, 11);
+            this.lblNameA.ForeColor = System.Drawing.Color.White;
+            this.lblNameA.Location = new System.Drawing.Point(99, 22);
             this.lblNameA.Name = "lblNameA";
             this.lblNameA.Size = new System.Drawing.Size(77, 13);
             this.lblNameA.TabIndex = 9;
@@ -95,7 +109,7 @@
             // 
             this.comboChannelB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboChannelB.FormattingEnabled = true;
-            this.comboChannelB.Location = new System.Drawing.Point(103, 30);
+            this.comboChannelB.Location = new System.Drawing.Point(99, 47);
             this.comboChannelB.Name = "comboChannelB";
             this.comboChannelB.Size = new System.Drawing.Size(163, 21);
             this.comboChannelB.TabIndex = 10;
@@ -104,7 +118,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 84);
+            this.label3.Location = new System.Drawing.Point(23, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 11;
@@ -113,7 +127,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(8, 81);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 13);
             this.label4.TabIndex = 12;
@@ -121,16 +136,16 @@
             // 
             // textResult
             // 
-            this.textResult.Location = new System.Drawing.Point(103, 111);
+            this.textResult.Location = new System.Drawing.Point(99, 78);
             this.textResult.Name = "textResult";
             this.textResult.Size = new System.Drawing.Size(162, 20);
-            this.textResult.TabIndex = 13;
+            this.textResult.TabIndex = 40;
             this.textResult.Text = "Math Result";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 58);
+            this.label5.Location = new System.Drawing.Point(23, 40);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 13);
             this.label5.TabIndex = 14;
@@ -139,7 +154,7 @@
             // lblMathMode
             // 
             this.lblMathMode.AutoSize = true;
-            this.lblMathMode.Location = new System.Drawing.Point(103, 58);
+            this.lblMathMode.Location = new System.Drawing.Point(111, 40);
             this.lblMathMode.Name = "lblMathMode";
             this.lblMathMode.Size = new System.Drawing.Size(40, 13);
             this.lblMathMode.TabIndex = 15;
@@ -147,11 +162,12 @@
             // 
             // btnExecuteClose
             // 
+            this.btnExecuteClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnExecuteClose.ForeColor = System.Drawing.Color.Black;
-            this.btnExecuteClose.Location = new System.Drawing.Point(13, 143);
+            this.btnExecuteClose.Location = new System.Drawing.Point(12, 269);
             this.btnExecuteClose.Name = "btnExecuteClose";
-            this.btnExecuteClose.Size = new System.Drawing.Size(109, 23);
-            this.btnExecuteClose.TabIndex = 16;
+            this.btnExecuteClose.Size = new System.Drawing.Size(116, 23);
+            this.btnExecuteClose.TabIndex = 50;
             this.btnExecuteClose.Text = "Execute and Close";
             this.btnExecuteClose.UseVisualStyleBackColor = true;
             this.btnExecuteClose.Click += new System.EventHandler(this.btnExecuteClose_Click);
@@ -161,32 +177,110 @@
             this.linkHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.linkHelp.AutoSize = true;
             this.linkHelp.LinkColor = System.Drawing.Color.Lime;
-            this.linkHelp.Location = new System.Drawing.Point(204, 146);
+            this.linkHelp.Location = new System.Drawing.Point(232, 272);
             this.linkHelp.Name = "linkHelp";
             this.linkHelp.Size = new System.Drawing.Size(59, 13);
-            this.linkHelp.TabIndex = 116;
+            this.linkHelp.TabIndex = 52;
             this.linkHelp.TabStop = true;
             this.linkHelp.Text = "Show Help";
             this.linkHelp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkHelp_LinkClicked);
+            // 
+            // lblParam
+            // 
+            this.lblParam.AutoSize = true;
+            this.lblParam.ForeColor = System.Drawing.Color.White;
+            this.lblParam.Location = new System.Drawing.Point(8, 46);
+            this.lblParam.Name = "lblParam";
+            this.lblParam.Size = new System.Drawing.Size(58, 13);
+            this.lblParam.TabIndex = 117;
+            this.lblParam.Text = "Parameter:";
+            // 
+            // textParameter
+            // 
+            this.textParameter.Location = new System.Drawing.Point(99, 43);
+            this.textParameter.Name = "textParameter";
+            this.textParameter.Size = new System.Drawing.Size(77, 20);
+            this.textParameter.TabIndex = 30;
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.ForeColor = System.Drawing.Color.White;
+            this.lblUnit.Location = new System.Drawing.Point(182, 46);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(26, 13);
+            this.lblUnit.TabIndex = 118;
+            this.lblUnit.Text = "Unit";
+            // 
+            // groupSingle
+            // 
+            this.groupSingle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupSingle.Controls.Add(this.lblChannel);
+            this.groupSingle.Controls.Add(this.lblName);
+            this.groupSingle.Controls.Add(this.lblParam);
+            this.groupSingle.Controls.Add(this.textParameter);
+            this.groupSingle.Controls.Add(this.lblUnit);
+            this.groupSingle.ForeColor = System.Drawing.Color.White;
+            this.groupSingle.Location = new System.Drawing.Point(14, 60);
+            this.groupSingle.Name = "groupSingle";
+            this.groupSingle.Size = new System.Drawing.Size(276, 76);
+            this.groupSingle.TabIndex = 119;
+            this.groupSingle.TabStop = false;
+            this.groupSingle.Text = " Single Channel ";
+            // 
+            // groupDual
+            // 
+            this.groupDual.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupDual.Controls.Add(this.label4);
+            this.groupDual.Controls.Add(this.textResult);
+            this.groupDual.Controls.Add(this.lblChannelB);
+            this.groupDual.Controls.Add(this.lblChannelA);
+            this.groupDual.Controls.Add(this.lblNameA);
+            this.groupDual.Controls.Add(this.comboChannelB);
+            this.groupDual.ForeColor = System.Drawing.Color.White;
+            this.groupDual.Location = new System.Drawing.Point(14, 142);
+            this.groupDual.Name = "groupDual";
+            this.groupDual.Size = new System.Drawing.Size(276, 114);
+            this.groupDual.TabIndex = 120;
+            this.groupDual.TabStop = false;
+            this.groupDual.Text = " Dual Channel ";
+            // 
+            // lblChannel
+            // 
+            this.lblChannel.AutoSize = true;
+            this.lblChannel.ForeColor = System.Drawing.Color.White;
+            this.lblChannel.Location = new System.Drawing.Point(8, 22);
+            this.lblChannel.Name = "lblChannel";
+            this.lblChannel.Size = new System.Drawing.Size(49, 13);
+            this.lblChannel.TabIndex = 41;
+            this.lblChannel.Text = "Channel:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(99, 22);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(77, 13);
+            this.lblName.TabIndex = 42;
+            this.lblName.Text = "Channel Name";
             // 
             // Mathematical
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(283, 178);
+            this.ClientSize = new System.Drawing.Size(304, 304);
+            this.Controls.Add(this.groupDual);
+            this.Controls.Add(this.groupSingle);
             this.Controls.Add(this.linkHelp);
             this.Controls.Add(this.btnExecuteClose);
             this.Controls.Add(this.lblMathMode);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textResult);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboChannelB);
-            this.Controls.Add(this.lblNameA);
-            this.Controls.Add(this.lblChannelB);
-            this.Controls.Add(this.lblChannelA);
             this.Controls.Add(this.comboMath);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnExecute);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -197,6 +291,10 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Math Operations";
+            this.groupSingle.ResumeLayout(false);
+            this.groupSingle.PerformLayout();
+            this.groupDual.ResumeLayout(false);
+            this.groupDual.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,5 +315,12 @@
         private System.Windows.Forms.Label lblMathMode;
         private System.Windows.Forms.Button btnExecuteClose;
         private System.Windows.Forms.LinkLabel linkHelp;
+        private System.Windows.Forms.Label lblParam;
+        private System.Windows.Forms.TextBox textParameter;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.GroupBox groupSingle;
+        private System.Windows.Forms.Label lblChannel;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.GroupBox groupDual;
     }
 }
